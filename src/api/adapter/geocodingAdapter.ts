d6e1cda82efbb1220cbc7468dcd6a0d1f16df2a7
Dario <31618416+dariomrk@users.noreply.github.com>
2022-06-19 19:45:09 +0200

    Fix Typo

--- src/api/adapter/geocodingAdapter.ts
+++ src/api/adapter/geocodingAdapter.ts
@@ -4,7 +4,7 @@ import { geocodingDataT, getGeocoding } from "../getGeocoding";
  * Fetches location data from https://openweathermap.org/api/geocoding-api based on the input argument.  
  * In case the HTTP request returns a 200 status, the function resolves and returns the most likely location.  
  * In case there is no valid location for the given input, null is returned.  
- * In case of an external or fetch error an error the function rejects and returns an error object.
+ * In case of an external or fetch error the function rejects and returns an error object.
  * @param apiKey OpenWeatherMap API key
  * @param input City name, country code divided by comma
  * @returns Promise with the most likely location for the given input string or null if there is no valid location
